aabf631417caedcab69a46c83b13ad6a67cc0b38
Ubuntu <ubuntu@ip-172-31-10-75.ap-south-1.compute.internal>
2017-06-19 12:22:10 +0000

    from ubuntu

--- backend/server/config/database.ts
+++ backend/server/config/database.ts
@@ -3,7 +3,7 @@
  */
 
 export class MyConfig{
-    #dbUrl : string = 'mongodb://localhost:27017/meanauth';
+    //dbUrl : string = 'mongodb://localhost:27017/meanauth';
     dbUrl : string = 'mongodb://gpankaj:onanai123@ds119750.mlab.com:19750/onanai';
     secretKey : string = 'secret';
 
