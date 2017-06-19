19473b07b1638d5a911d90e292e4e7681d7de1e4
EC2 Default User <ec2-user@ip-172-31-7-242.ap-south-1.compute.internal>
2017-06-19 11:16:07 +0000

    From AWS - 1

--- backend/server/config/database.ts
+++ backend/server/config/database.ts
@@ -3,7 +3,8 @@
  */
 
 export class MyConfig{
-    dbUrl : string = 'mongodb://localhost:27017/meanauth';
+    #dbUrl : string = 'mongodb://localhost:27017/meanauth';
+    dbUrl : string = 'mongodb://gpankaj:onanai123@ds119750.mlab.com:19750/onanai';
     secretKey : string = 'secret';
 
     constructor() {
