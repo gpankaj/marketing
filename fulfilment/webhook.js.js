926fff589fe75b2a7d4f9a0c868f78fed3b4e485
Pankaj Gupta <contact.gpankaj@gmail.com>
2017-06-23 21:51:05 +0530

    Added Fulfilment

---
+++ fulfilment/webhook.js.js
@@ -0,0 +1,24 @@
+/**
+ * Created by pg942665 on 6/23/2017.
+ */
+'use strict';
+var Restify = require('restify');
+var server = Restify.createServer({
+    name: "fulfilment"
+});
+var PORT = process.PORT || 5000;
+server.use(Restify.bodyParser());
+server.use(Restify.jsonp());
+//Route Handlers
+server.post('/', function (req, res, next) {
+    var _a = req.body, status = _a.status, result = _a.result;
+    console.log(result);
+    return next();
+});
+server.get('/', function (req, res, next) {
+    console.log("Received ", JSON.stringify(req.parameters));
+    return next();
+});
+server.listen(PORT, function () {
+    console.log("Server listening on ", PORT);
+});
---
+++ fulfilment/webhook.js.ts
@@ -0,0 +1,36 @@
+/**
+ * Created by pg942665 on 6/23/2017.
+ */
+'use strict';
+
+const Restify = require('restify');
+
+const server = Restify.createServer({
+    name: "fulfilment"
+});
+
+const PORT = process.PORT || 5000;
+
+server.use(Restify.bodyParser());
+server.use(Restify.jsonp());
+
+
+//Route Handlers
+server.post('/', (req,res,next)=>{
+
+    let  {status, result} = req.body;
+
+    console.log(result);
+    return next();
+});
+
+server.get('/', (req,res,next)=>{
+    console.log("Received " , JSON.stringify(req.parameters));
+    return next();
+});
+
+
+
+server.listen(PORT,()=>{
+    console.log("Server listening on " , PORT);
+})
